fc41853bb59e5aea0f4e808b852c7903e2405f63
MutisoMuli <mulimutiso525@gmail.com>
2023-11-13 23:21:20 +0300

    Done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-wget -P
+wget -P .. https://raw.githubusercontent.com/MutisoMuli/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
